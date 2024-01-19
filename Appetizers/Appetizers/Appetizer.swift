c99037b83721ca02ef6e47d22dc0fc9619146c66
Matt <matt.j.zbik@gmail.com>
2024-01-19 14:33:28 -0700

    feat: Created Basic Appetizer Model

---
+++ Appetizers/Appetizers/Appetizer.swift
@@ -0,0 +1,36 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by Matthew Zbik on 2024-01-19.
+//
+
+import Foundation
+
+struct Appetizer: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct AppetizerResponse {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    static let sampleAppetizer = Appetizer(id: 01,
+                                           name: "Test Appetizer",
+                                           description: "This is the description for appetizer, it's yummy",
+                                           price: 9.99,
+                                           imageURL: "asian-flank-steak", 
+                                           calories: 99,
+                                           protein: 99,
+                                           carbs: 99)
+    
+    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+}
