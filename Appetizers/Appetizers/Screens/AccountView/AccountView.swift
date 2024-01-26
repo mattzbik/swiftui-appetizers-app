f633b9d41afeb2e9afbf7a1a13babb8e2e499c42
Matt <matt.j.zbik@gmail.com>
2024-01-26 13:45:51 -0700

    feat: Updating Account View

--- Appetizers/Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Appetizers/Screens/AccountView/AccountView.swift
@@ -8,10 +8,44 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthdate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+    
     var body: some View {
         NavigationStack {
-            Text("Account")
-                .navigationTitle("😂 Account")
+            Form {
+                Section(content: {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled()
+                    DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+                    
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                }, header: {
+                    Text("Personal Info")
+                })
+                
+                Section(content: {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                }, header: {
+                    Text("Requests")
+                })
+                .tint(.brandPrimary)
+            }
+            .navigationTitle("😂 Account")
         }
     }
 }
